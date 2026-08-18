5db81d6e2bc4f59e65fb3728940e06c029bdd881
Azazel Castro <azazel.developer@gmail.com>
2026-08-18 19:22:28 -0300

    WIP
    ainda adding context e provider

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,17 @@
 import { Stack } from "expo-router";
 
+
+import { quiz } from "../data/quizzes";
+import { QuizProvider } from "../contexts/QuizProvider";
+
 export default function RootLayout() {
-  return (
-		<Stack screenOptions={{ headerShown: false }}>
-			<Stack.Screen name="index" />
-			<Stack.Screen name="quiz" />
-			<Stack.Screen name="results" />
-		</Stack>
+	return (
+		<QuizProvider quiz={quiz}>
+			<Stack screenOptions={{ headerShown: false }}>
+				<Stack.Screen name="index" />
+				<Stack.Screen name="quiz" />
+				<Stack.Screen name="results" />
+			</Stack>
+		</QuizProvider>
 	);
 }
---
+++ src/contexts/QuizProvider.tsx
@@ -0,0 +1,57 @@
+import { ReactNode, useState } from "react";
+
+import { Quiz, UserAnswer, AnswerId } from "../types/quiz";
+import { QuizContext } from "./QuizContext";
+
+interface QuizProviderProps {
+	quiz: Quiz;
+	children: ReactNode;
+}
+
+export function QuizProvider({ quiz, children }: QuizProviderProps) {
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [score, setScore] = useState(0);
+	const [userAnswers, setUserAnswers] = useState<UserAnswer[]>([]);
+
+	const answerQuestion = (answerId: AnswerId) => {
+		const question = quiz.questions[currentQuestion];
+
+		const isCorrect = answerId === question.correctAnswer;
+
+		const userAnswer: UserAnswer = {
+			questionId: question.id,
+			selectedAnswer: answerId,
+			correct: isCorrect,
+		};
+
+		setUserAnswers((previousAnswers) => [...previousAnswers, userAnswer]);
+
+		if (isCorrect) {
+			setScore((previousScore) => previousScore + 1);
+		}
+
+		if (currentQuestion < quiz.questions.length - 1) {
+			setCurrentQuestion((previousQuestion) => previousQuestion + 1);
+		}
+	};
+
+	const resetQuiz = () => {
+		setCurrentQuestion(0);
+		setScore(0);
+		setUserAnswers([]);
+	};
+
+	return (
+		<QuizContext.Provider
+			value={{
+				currentQuestion,
+				score,
+				userAnswers,
+				answerQuestion,
+				resetQuiz,
+			}}
+		>
+			{children}
+		</QuizContext.Provider>
+	);
+}
