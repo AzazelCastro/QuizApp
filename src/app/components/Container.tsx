5149937c9a96692576851753d5d0a57bea08cb31
Azazel Castro <azazel.developer@gmail.com>
2026-08-27 11:29:03 -0300

    alterando container para permitir scroll

--- src/app/components/Container.tsx
+++ src/app/components/Container.tsx
@@ -1,23 +1,45 @@
 import { ReactNode } from "react";
-import { StyleSheet } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { theme } from "../theme";
+import { theme } from "@/theme";
 
 interface props {
 	children: ReactNode;
+	scrollable?: boolean;
 }
 
-export default function Container({ children }: props) {
-	return <SafeAreaView style={styles.container}>{children}</SafeAreaView>;
+export default function Container({ children, scrollable = true }: props) {
+	return (
+		<SafeAreaView style={styles.container}>
+			{scrollable ? (
+				<ScrollView
+					contentContainerStyle={styles.scrollContent}
+					showsVerticalScrollIndicator={true}
+				>
+					{children}
+				</ScrollView>
+			) : (
+				<View style={styles.fixedContent}>{children}</View>
+			)}
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+		backgroundColor: theme.colors.background,
+	},
+	scrollContent: {
+		flexGrow: 1,
+		padding: 20,
 		justifyContent: "center",
 		alignItems: "center",
+	},
+	fixedContent: {
+		flex: 1,
 		padding: 20,
-		backgroundColor: theme.colors.background,
-		color: theme.colors.text,
+		justifyContent: "center",
+		alignItems: "center",
 	},
 });
